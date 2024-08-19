866d6ff49eff0c19e1ffede1d3f082ff2e8ec218
Tallhaa <talhajutt4880@gmail.com>
2024-08-19 18:39:27 +0500

    update package.json file and app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,11 @@ const cors = require("cors");
 const router = require("./routes/router");
 const port = 4004;
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://crud-cloudinary-client-mzezii3ll-tallhaas-projects.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use(router);
