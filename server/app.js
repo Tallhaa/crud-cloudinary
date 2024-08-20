86f43f437229ceed87e24db1ed4a3e2e1defaab3
Tallhaa <talhajutt4880@gmail.com>
2024-08-20 10:52:10 +0500

    removed vecel.json

--- server/app.js
+++ server/app.js
@@ -7,11 +7,7 @@ const cors = require("cors");
 const router = require("./routes/router");
 const port = 4004;
 
-app.use(cors({
-    origin: ["https://crud-cloudinary-client-mzezii3ll-tallhaas-projects.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
